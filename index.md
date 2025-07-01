c451e94bd147a8fae977b555dece6ed168282cec
Gandhar S. Purandare <gandharpurandare@gmail.com>
2025-07-01 08:30:51 +0530

    Update and rename index.md to index.py

--- index.md
+++
@@ -1,5 +0,0 @@
----
-title: 'Gandhar Shrikant Purandare'
-
-
----
---
+++ index.py
@@ -0,0 +1,9 @@
+---python
+print('Gandhar Shrikant Purandare ')
+print('\n')
+print('My Certificates')
+print(' 1. Matlab Onramp Certificate :- (link)[https://matlabacademy.mathworks.com/progress/share/certificate.html?id=908a995c-a7e8-4fce-a72e-52c01ec2dae3&]
+          \n
+          2. Introduction to Numpy :- (link)[https://simpli-web.app.link/e/gIIFpALzDUb]')
+
+---
